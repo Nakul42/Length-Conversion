5b5b06e10a48944443e50d44976c00a05f325ae6
Nakul Varma <varmanakul42@gmail.com>
2024-12-13 22:56:35 +0530

    All changes done

--- src/Convertor.js
+++ src/Convertor.js
@@ -1,15 +1,17 @@
 import { useState } from 'react';
-const Convertor = () => {  //mm to centimeter , inches to centimeter , feets to centimeter (vise verca)
+
+const Convertor = () => { // mm to cm, inches to cm, feet to cm (vice versa)
     const [leftValue, setLeftValue] = useState(1);
     const [leftUnit, setLeftUnit] = useState("meter");
     const [rightValue, setRightValue] = useState(100);
     const [rightUnit, setRightUnit] = useState("centimeter");
 
     const conversionRates = {
-        meter: { meter: 1, centimeter: 100, millimeter: 1000, inch: 39.3701 },
-        centimeter: { meter: 0.01, centimeter: 1, millimeter: 10, inch: 0.393701 },
-        millimeter: { meter: 0.001, centimeter: 0.1, millimeter: 1, inch: 0.0393701 },
-        inch: { meter: 0.0254, centimeter: 2.54, millimeter: 25.4, inch: 1 },
+        meter: { meter: 1, centimeter: 100, millimeter: 1000, inch: 39.3701, foot: 3.28084 },
+        centimeter: { meter: 0.01, centimeter: 1, millimeter: 10, inch: 0.393701, foot: 0.0328084 },
+        millimeter: { meter: 0.001, centimeter: 0.1, millimeter: 1, inch: 0.0393701, foot: 0.00328084 },
+        inch: { meter: 0.0254, centimeter: 2.54, millimeter: 25.4, inch: 1, foot: 0.0833333 },
+        foot: { meter: 0.3048, centimeter: 30.48, millimeter: 304.8, inch: 12, foot: 1 }, 
     };
 
     const convert = (value, fromUnit, toUnit) => {
@@ -41,44 +43,48 @@ const Convertor = () => {  //mm to centimeter , inches to centimeter , feets to 
         setRightValue(convert(leftValue, leftUnit, newUnit)); // Update right value
     };
 
-
     return (
         <div className="container">
-          <div className="convert">
-            <div className="left-div">
-              <input
-                type="number"
-                min={1}
-                max={100}
-                value={leftValue}
-                onChange={handleLeftValueChange}
-              />
-              <select value={leftUnit} onChange={handleLeftUnitChange}>
-                <option value="meter">Meter</option>
-                <option value="centimeter">Centimeter</option>
-                <option value="millimeter">Millimeter</option>
-                <option value="inch">Inch</option>
-              </select>
+            <div className='heading'>
+                Unit Convertor
             </div>
-            <div className="equal-sign">=</div>
-            <div className="right-div">
-              <input
-                type="number"
-                min={1}
-                max={100}
-                value={rightValue}
-                onChange={handleRightValueChange}
-              />
-              <select value={rightUnit} onChange={handleRightUnitChange}>
-                <option value="meter">Meter</option>
-                <option value="centimeter">Centimeter</option>
-                <option value="millimeter">Millimeter</option>
-                <option value="inch">Inch</option>
-              </select>
+            <div className="convert">
+                <div className="left-div">
+                    <input
+                        type="number"
+                        min={1}
+                        max={100}
+                        value={leftValue}
+                        onChange={handleLeftValueChange}
+                    />
+                    <select value={leftUnit} onChange={handleLeftUnitChange}>
+                        <option value="meter">Meter</option>
+                        <option value="centimeter">Centimeter</option>
+                        <option value="millimeter">Millimeter</option>
+                        <option value="inch">Inch</option>
+                        <option value="foot">Foot</option> 
+                    </select>
+                </div>
+                <div className="equal-sign">=</div>
+                <div className="right-div">
+                    <input
+                        type="number"
+                        min={1}
+                        max={100}
+                        value={rightValue}
+                        onChange={handleRightValueChange}
+                    />
+                    <select value={rightUnit} onChange={handleRightUnitChange}>
+                        <option value="meter">Meter</option>
+                        <option value="centimeter">Centimeter</option>
+                        <option value="millimeter">Millimeter</option>
+                        <option value="inch">Inch</option>
+                        <option value="foot">Foot</option>
+                    </select>
+                </div>
             </div>
-          </div>
         </div>
-      );
-    };
-    
+    );
+};
+
 export default Convertor;
